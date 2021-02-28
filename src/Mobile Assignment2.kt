743f6342ed93565034b8f1e611098257b644a17c
student <rahmafaizyusuf@gmail.com>
2021-02-28 17:31:50 +0300

    Mobile Assignment2

---
+++ src/Mobile Assignment2.kt
@@ -0,0 +1,27 @@
+fun main() {
+println("Hello Rahma")
+    division()
+    addition()
+    println("true")
+}
+fun Rahma(){
+    println("Hello Rahma")
+}
+fun division(){
+    var num1=25
+    var num2=4
+    var modulus=num1%num2
+    println(modulus)
+}
+fun addition(){
+    var num1=5
+    var num2=10
+    var num3=20
+    var num4=25
+    var addition=num1+num2+num3+num4
+    println(addition)
+}
+fun myself(){
+    var myself="short"
+    println("true")
+}
